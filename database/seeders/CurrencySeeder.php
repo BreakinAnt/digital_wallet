4c42b0ae906e0f48847d83cb514a2a184f50ebe3
Felipe <122118266+FelipeCarneiroKCX@users.noreply.github.com>
2025-03-19 18:31:43 -0300

    Add CurrencySeeder to populate currency data in the database

---
+++ database/seeders/CurrencySeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CurrencySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        \DB::table('currencies')->insert([
+            ['name' => 'US Dollar', 'code' => 'USD', 'symbol' => '$'],
+            ['name' => 'Euro', 'code' => 'EUR', 'symbol' => '€'],
+            ['name' => 'British Pound', 'code' => 'GBP', 'symbol' => '£'],
+            ['name' => 'Japanese Yen', 'code' => 'JPY', 'symbol' => '¥'],
+            ['name' => 'Australian Dollar', 'code' => 'AUD', 'symbol' => 'A$'],
+            ['name' => 'Canadian Dollar', 'code' => 'CAD', 'symbol' => 'C$'],
+            ['name' => 'Swiss Franc', 'code' => 'CHF', 'symbol' => 'CHF'],
+            ['name' => 'Chinese Yuan', 'code' => 'CNY', 'symbol' => '¥'],
+            ['name' => 'Swedish Krona', 'code' => 'SEK', 'symbol' => 'kr'],
+            ['name' => 'New Zealand Dollar', 'code' => 'NZD', 'symbol' => 'NZ$'],
+            ['name' => 'Mexican Peso', 'code' => 'MXN', 'symbol' => '$'],
+            ['name' => 'Singapore Dollar', 'code' => 'SGD', 'symbol' => 'S$'],
+            ['name' => 'Hong Kong Dollar', 'code' => 'HKD', 'symbol' => 'HK$'],
+            ['name' => 'Norwegian Krone', 'code' => 'NOK', 'symbol' => 'kr'],
+            ['name' => 'South Korean Won', 'code' => 'KRW', 'symbol' => '₩'],
+            ['name' => 'Brazilian Real', 'code' => 'BRL', 'symbol' => 'R$'],
+        ]);
+    }
+}
