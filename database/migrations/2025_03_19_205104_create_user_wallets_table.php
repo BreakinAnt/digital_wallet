9181ec39338d8226ee583ae904c78b5974f8c8f9
Felipe <122118266+FelipeCarneiroKCX@users.noreply.github.com>
2025-03-19 18:14:09 -0300

    Add Currency, UserTransaction, and UserWallet models with migrations

---
+++ database/migrations/2025_03_19_205104_create_user_wallets_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_wallets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('currency_id')->references('currencies');
+            $table->integer('balance')->default(0);
+            $table->boolean('active')->default(1);
+            $table->string('type', 20)->default('personal');
+            $table->index(['user_id']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_wallets');
+    }
+};
---
+++ database/migrations/2025_03_19_205115_create_user_transactions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('users');
+            $table->foreignId('wallet_id')->references('user_wallets');
+            $table->foreignId('target_walled_id')->references('user_wallets')->nullable();
+            $table->foreignId('currency_id')->references('currencies');
+            $table->integer('amount');
+            $table->string('type', 20)->default('deposit');
+            $table->string('status', 20)->default('pending');
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamp('cancelled_at')->nullable();
+            $table->softDeletes();
+            $table->index(['user_id', 'wallet_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_transactions');
+    }
+};
