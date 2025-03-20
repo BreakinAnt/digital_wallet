74501f0854fbc386642745f4794e6c11c78f11d3
Felipe <122118266+FelipeCarneiroKCX@users.noreply.github.com>
2025-03-20 02:14:59 -0300

    Add fillable properties and relationship method to TransactionStatus model

--- app/Models/TransactionStatus.php
+++ app/Models/TransactionStatus.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class TransactionStatus extends Model
 {
-    //
+    protected $fillable = [
+        'transaction_id',
+        'name',
+    ];
+
+    public function transaction()
+    {
+        return $this->belongsTo(UserTransaction::class, 'transaction_id');
+    }
 }
