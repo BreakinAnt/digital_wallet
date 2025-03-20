a03a5148952f0e0c38d86ba3de356e052c457c89
Felipe <122118266+FelipeCarneiroKCX@users.noreply.github.com>
2025-03-20 02:47:56 -0300

    Refactor TransactionResource to format amount and timestamps, and enhance currency representation

--- app/Resources/TransactionResource.php
+++ app/Resources/TransactionResource.php
@@ -3,6 +3,7 @@
 namespace App\Resources;
 
 use App\Repositories\UserTransactionRepository;
+use DateTime;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TransactionResource extends JsonResource
@@ -18,16 +19,16 @@ class TransactionResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'amount' => $this->amount,
-            'type' => $this->type,
-            'status' => new TransactionStatusResource($this->transactionRepository->getCurrentStatus($this->resource)),
-            'currency' => $this->currency,
-            'created_at' => $this->created_at,
-            'confirmed_at' => $this->confirmed_at,
-            'cancelled_at' => $this->cancelled_at,
-            'user' => new UserResource($this->user),
-            'target_user' => new UserResource($this->targetUser),
+            'id'            => $this->id,
+            'amount'        => $this->amount / 100,
+            'type'          => $this->type,
+            'status'        => new TransactionStatusResource($this->transactionRepository->getCurrentStatus($this->resource)),
+            'currency'      => new CurrencyResource($this->currency),
+            'created_at'    => (new DateTime($this->created_at))->format('Y-m-d H:i:s'),
+            'completed_at'  => (new DateTime($this->completed_at))->format('Y-m-d H:i:s'),
+            'cancelled_at'  => (new DateTime($this->cancelled_at))->format('Y-m-d H:i:s'),
+            'user'          => new UserResource($this->user),
+            'target_user'   => new UserResource($this->targetUser),
         ];
     }
 }
