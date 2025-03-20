232a7acf027dab893b42487e057b7bc9e77d5db9
Felipe <122118266+FelipeCarneiroKCX@users.noreply.github.com>
2025-03-20 02:56:21 -0300

    Add refunding and refunded statuses to TransactionStatusEnum and migration

--- app/Enums/TransactionStatusEnum.php
+++ app/Enums/TransactionStatusEnum.php
@@ -7,4 +7,6 @@ enum TransactionStatusEnum: string
     case PENDING = 'pending';
     case COMPLETED = 'completed';
     case CANCELLED = 'cancelled';
+    case REFUNDING = 'refunding';
+    case REFUNDED = 'refunded';
 }
--- database/migrations/2025_03_19_212433_create_transaction_statuses_table.php
+++ database/migrations/2025_03_19_212433_create_transaction_statuses_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('transaction_statuses', function (Blueprint $table) {
             $table->id();
             $table->foreignId('transaction_id')->references('id')->on('user_transactions');
-            $table->enum('name', ['pending', 'completed', 'cancelled']);
+            $table->enum('name', ['pending', 'completed', 'cancelled', 'refunding', 'refunded']);
             $table->softDeletes();
             $table->index(['name']);
             $table->timestamps();
